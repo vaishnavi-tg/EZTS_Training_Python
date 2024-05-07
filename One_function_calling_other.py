a5460deec0d6e2aa1b89213ce449181a6be732b2
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-05-07 16:02:20 +0530

    Added few files

---
+++ One_function_calling_other.py
@@ -0,0 +1,21 @@
+# Functions ---lines for specific task
+
+# 2 types of functions
+#1.Inbuilt-->print,list,str
+# user defiened functiond---definition and call
+
+
+def Hyd():
+    print("Biryani")
+    Ballari()
+def Ballari():
+    print("Mirchi")
+    Banaras()
+def Banaras():
+    print("Litti Chokha")
+    Banglore()
+def Banglore():
+    print("Benne Dosa")
+
+Hyd()    #----This function prints what is their in it and calls otheer function         
+        
---
+++ Operators.py
@@ -0,0 +1,57 @@
+# Note:When we use modulo operator and the numerator is smallar and denominator is bigger then answer is numerator and if vv then take remainder
+# Precedence Rule 
+# power,modulo,floor divison,divison,addition,subtraction
+# Division is given by associative rule
+# Assignment operators
+a=10
+a=100
+a=1.8
+print(a)
+# When we create a object in python then 2 things will get created 
+# the object and the reference of the object
+
+
+# Number of references
+a=10   # --1
+b=10   # --2
+a=10   # --1
+b=20   # --0
+
+# Garbage collector collects the previous references and the last one is taken as the main reference in case of multiple references
+# Short hand or Compound operator--->Assignment
+print("Prathyusha">"sidrampura kumar")
+# Unicode is a number reperenstation of different characters of the different languages
+# "ALWAYS STRING COMPARISION HAPPENSS ACCORDING TO THE UNICODES OF CHARACTERS"
+print(9>10<11<12>15)
+
+
+# In logical operators we do many conditions
+# and,or,not
+#In and we ll get the last thing and in or v ll get 1st 
+print(8 and 10)
+print(8 and 10 and 0.0001 and "hi")
+print("Rama"or"krishna")
+print(1 and 0 or 1 and 6 or 7)
+
+
+
+# Bitwise
+# [&,^,,>>,|,<<,~]
+print(123&456)
+print(789|123)
+
+
+
+# Formula IMPORTANT
+# ~n=-[n+1]
+# ~15=-16
+# ~-16===-[-16+1]
+
+
+
+
+# Identity and membership operator
+# conditional statements
+# True---->non zero--->1,8.9,[8],6+3j
+# False---->zero--->0,0.0,None
+# [],(),{},' '
