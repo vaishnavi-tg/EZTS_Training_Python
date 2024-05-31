fa4eb7f232272bdfc6f24c74e1fecce8cc3618d8
Vaishnavi TG <Vaishnavitg2@gmail.com>
2024-05-31 15:05:37 +0530

    Added few files

---
+++ Even_odd.py
@@ -0,0 +1,5 @@
+num=int(input("Enter the number\n"))
+if num%2==0:
+    print("The number is Even")
+elif num%2!=0:
+    print("The number is Odd")    
---
+++ Find_first_mid_last_characterofname.py
@@ -0,0 +1,5 @@
+name=input("Enter the name\n")
+print(name[0],"IS the first character of the name")
+print(name[-1],"Is the last character of the name")
+mid_index=len(name)//2
+print(mid_index,'Is the the middle charcater of the name')
